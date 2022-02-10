8628fedbd2c4ed23f50c73d74986ac480f59bbc7
letmp <le_tmp@gmx.de>
2022-02-10 23:56:21 +0100

    [updated] folder structure

--- webapp/buildscript/build.ps1
+++ webapp/buildscript/build.ps1
@@ -1,10 +1,10 @@
-$fileindex = "../dev/index.html"
+$fileindex = "../index.html"
 $index = Get-Content $fileindex -Encoding UTF8 -Raw
 
-$filecss = "../dev/droid.css"
+$filecss = "../droid.css"
 $css = Get-Content $filecss -Encoding UTF8 -Raw
 
-$filejs = "../dev/droid.js"
+$filejs = "../droid.js"
 $js = Get-Content $filejs -Encoding UTF8 -Raw
 
 $REPLACE_CSS_FROM = '<link rel=\"stylesheet\" href=\"droid.css\">'
@@ -12,10 +12,13 @@ $REPLACE_CONTENT_FROM = '<div id=\"code\" class=\"invisible\">'
 $REPLACE_CONTENT_TO = '<div id="code" class="invisible">$droidcomponents'
 $REPLACE_JS_FROM = '<script src=\"droid.js\"><\/script>'
 
-$filetoStandalone = "../droid.html"
 $filetoSublime = "../../language-extensions/sublime/droid-lang/buildscript/droid.html"
+$filetoSublimeStandalone = "../../language-extensions/sublime/droid.html"
 $filetoVscode = "../../language-extensions/vscode/droid-lang/buildscript/droid.html"
+$filetoVscodeStandalone = "../../language-extensions/vscode/droid.html"
 
-($index -replace $REPLACE_CSS_FROM,"<style>$css</style>" -replace $REPLACE_JS_FROM,"<script>$js</script>") | Out-File -encoding ASCII $filetoStandalone
 ($index -replace $REPLACE_CONTENT_FROM,$REPLACE_CONTENT_TO -replace $REPLACE_CSS_FROM,"<style>$css</style>" -replace $REPLACE_JS_FROM,"<script>$js</script>") | Out-File -encoding ASCII $filetoSublime
 ($index -replace $REPLACE_CONTENT_FROM,$REPLACE_CONTENT_TO -replace $REPLACE_CSS_FROM,"<style>$css</style>" -replace $REPLACE_JS_FROM,"<script>$js</script>") | Out-File -encoding ASCII $filetoVscode
+
+($index -replace $REPLACE_CSS_FROM,"<style>$css</style>" -replace $REPLACE_JS_FROM,"<script>$js</script>") | Out-File -encoding ASCII $filetoSublimeStandalone
+($index -replace $REPLACE_CSS_FROM,"<style>$css</style>" -replace $REPLACE_JS_FROM,"<script>$js</script>") | Out-File -encoding ASCII $filetoVscodeStandalone
