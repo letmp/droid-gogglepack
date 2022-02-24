e5bdc3c8178fc1c13468f9984a0b5e766d203b9b
letmp <le_tmp@gmx.de>
2022-02-25 00:30:57 +0100

    [added] sendmidi functionality -> sublime/win

--- language-extensions/sublime/droid-lang/buildscript/droidcopy.ps1
+++ language-extensions/sublime/droid-lang/buildscript/droidcopy.ps1
@@ -6,7 +6,9 @@ if(![System.IO.File]::Exists($filefrom)){
     Write-Host -ForegroundColor Yellow "Save your file first!"
     Exit
 }
-# parse droidcomponents
+
+# ========= parse droidcomponents and write html file =========
+Write-Host -ForegroundColor Yellow "Building HTML File"
 $droidcontent = Get-Content $filefrom -Encoding UTF8 -Raw
 $pattern = "(# BEGIN DROIDCOMPONENT)(.|\n)*?(END DROIDCOMPONENT)"
 $droidcomponents =  Select-String -InputObject $droidcontent -Pattern $pattern -AllMatches | 
@@ -24,3 +26,24 @@ foreach($line in $templatecontent) {
 
 # write the final html file
 Set-Content $fileto $data
+
+
+# ========= send filecontent via sysex to x7 expander ========= 
+
+$x7token = "# X7 send"
+$containsWord = $droidcontent | %{$_ -match $x7token}
+If($containsWord -contains $true)
+{
+    Write-Host -ForegroundColor Yellow "Sending data via sysex"
+
+    $fileSysexHead= Get-Content "$PSScriptRoot/sendmidi/win/sysexhead.txt"
+    $fileSysexTail= Get-Content "$PSScriptRoot/sendmidi/win/sysextail.txt"
+    
+    $fileSysexTmp= "$PSScriptRoot/sendmidi/tmp.syx"
+    Set-Content $fileSysexTmp "$fileSysexHead $droidcontent $fileSysexTail"
+
+    $smbin = "$PSScriptRoot/sendmidi/win/sendmidi.exe"
+    & $smbin dev x7 syf $fileSysexTmp
+
+    Remove-Item $fileSysexTmp
+}
