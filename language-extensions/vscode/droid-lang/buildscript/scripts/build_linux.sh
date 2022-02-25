1f236fa1e959e6b8f2261c4f396c1127b9566cc3
letmp <le_tmp@gmx.de>
2022-02-25 15:35:14 +0100

    [updated] sendmidi functionality -> vscode/sublime - osx/linux

--- language-extensions/vscode/droid-lang/buildscript/scripts/build_linux.sh
+++ language-extensions/vscode/droid-lang/buildscript/scripts/build_linux.sh
@@ -2,7 +2,21 @@
 FILE_FROM="$1"
 FILE_TO="$2"
 FILE_TEMPLATE="$3"
+
+
+# ========= parse droidcomponents and write html file =========
+
+echo "Building HTML File"
 lines="$(sed -n '/# BEGIN/,/# END/p' $FILE_FROM)"
 cp $FILE_TEMPLATE $FILE_TO
 matchstring='\$droidcomponents'
 perl -i -pe "s/$matchstring/$lines/g" $FILE_TO
+
+
+# ========= send filecontent via sysex to x7 expander ========= 
+
+if grep -q '# x7 send' "$FILE_FROM"; then
+  echo "Sending data via sysex"
+  cd $(dirname $0)
+  (cd ../sendmidi/mac/ ; sh droidpatch $FILE_FROM)
+fi
