5dbf349d58e86f90354536ad9c57ed0b03b977de
letmp <le_tmp@gmx.de>
2022-03-02 10:23:17 +0100

    [bugfix] correct quotation of paths with spaces

--- language-extensions/sublime/droid-lang/buildscript/scripts/build_osx.sh
+++ language-extensions/sublime/droid-lang/buildscript/scripts/build_osx.sh
@@ -3,14 +3,13 @@ FILE_FROM="$1"
 FILE_TO="$2"
 FILE_TEMPLATE="$3"
 
-
 # ========= parse droidcomponents and write html file =========
 
 echo "Building HTML File"
-lines="$(sed -n '/# BEGIN/,/# END/p' $FILE_FROM)"
-cp $FILE_TEMPLATE $FILE_TO
+lines="$(sed -n '/# BEGIN/,/# END/p' "$FILE_FROM")"
+cp "$FILE_TEMPLATE" "$FILE_TO"
 matchstring='\$droidcomponents'
-perl -i -pe "s/$matchstring/$lines/g" $FILE_TO
+perl -i -pe "s/$matchstring/$lines/g" "$FILE_TO"
 
 
 # ========= send filecontent via sysex to x7 expander ========= 
@@ -18,10 +17,10 @@ perl -i -pe "s/$matchstring/$lines/g" $FILE_TO
 if grep -q '# x7 send' "$FILE_FROM"; then
   echo "Sending data via sysex"
   cd $(dirname $0)
-  dev="$(sed -n '/# x7 send/p' $FILE_FROM)"
+  dev="$(sed -n '/# x7 send/p' "$FILE_FROM")"
   dev=`echo "$dev" | cut -d'"' -f 2`
   if [[ $dev == *"x7 send"* ]]; then
     dev=x7 #no custom device name was set - set x7 as default
   fi
-  (cd ../sendmidi/mac/ ; sh sendpatch $FILE_FROM "$dev")
+  (cd ../sendmidi/mac/ ; sh sendpatch "$FILE_FROM" "$dev")
 fi
